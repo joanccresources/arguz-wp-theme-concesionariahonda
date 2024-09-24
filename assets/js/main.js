69b6413d3f3b98b36e095d9355f179567034308c
Joan C <joan0492@gmail.com>
2024-09-24 16:40:13 -0500

    Primera parte del footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,63 @@
   const urlAssets =
     "https://concesionariahonda.sorsa.pe/wp-content/themes/eura-child-sorsa/assets";
 
+  const changeFormContactFooter = () => {
+    const form = document.querySelector("form:has(>.form-footer-contact)");
+    if (!form) return;
+
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.attributeName === "class") {
+          const classList = form.classList;
+          if (classList.contains("invalid")) {
+            console.log("El formulario es inválido.");
+            const newForm = document.querySelector(
+              "form.invalid:has(>.form-footer-contact)"
+            );
+            if (!newForm) return;
+            setTimeout(() => {
+              const box1 = newForm.querySelector(".wpcf7-not-valid-tip");
+              if (!box1) return;
+              box1.innerHTML = "Por favor, rellene este campo.";
+
+              const box2 = newForm.querySelector(".wpcf7-response-output");
+              if (!box2) return;
+              if (
+                box2.textContent ===
+                "One or more fields have an error. Please check and try again."
+              ) {
+                box2.textContent =
+                  "Hay un error en uno o más campos. Por favor, compruébelo y vuelva a intentarlo.";
+              }
+            }, 150);
+          } else if (classList.contains("sent")) {
+            console.log("El formulario se envió con éxito.");
+            const newForm = document.querySelector(
+              "form.sent:has(>.form-footer-contact)"
+            );
+            if (!newForm) return;
+            setTimeout(() => {
+              const box2 = newForm.querySelector(".wpcf7-response-output");
+              if (!box2) return;
+              if (
+                box2.textContent ===
+                "Thank you for your message. It has been sent."
+              ) {
+                box2.textContent = "Gracias por tu mensaje. Ha sido enviado.";
+              }
+            }, 150);
+          }
+        }
+      });
+    });
+    observer.observe(form, { attributes: true });
+  };
+
   const addButtonInHeader = () => {
     const container = document.querySelector(
       "#navbar #menu-primary-menu + .others-option"
     );
     if (!container) return;
-    console.log(container);
     container.innerHTML = `
       <button class="btn-menu">
         <img src="${urlAssets}/img/menu-burger.svg" alt="menu-icon" class="btn-menu__img"/>
@@ -20,6 +71,7 @@
 
   const initDomReady = () => {
     addButtonInHeader();
+    changeFormContactFooter();
   };
 
   document.addEventListener("DOMContentLoaded", initDomReady);
---
+++ footer.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * The template for displaying the footer
+ * @package Eura
+ */
+
+// Footer Content
+// eura_theme_footer_content();
+// Back To Top
+eura_footer_back_to_top();
+
+wp_footer();
+?>
+
+<footer class="container-fluid footer" id="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <figure>
+          <a href="<?= home_url(); ?>">
+            <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/sorsa-motors.svg" alt="Logo">
+          </a>
+        </figure>
+        <p class="text-white">Somos un grupo empresarial familiar con casi 30 años en el sector automotriz, comprometidos con el desarrollo sotenible.</p>
+      </div>
+      <div class="col-md-3">
+        <ul class="list-slugs">
+          <li class="list-slugs__item">
+            <a href="#0" class="list-slugs__link">Concesionaria Honda</a>
+          </li>
+          <li class="list-slugs__item">
+            <a href="#0" class="list-slugs__link">Motos Honda</a>
+          </li>
+          <li class="list-slugs__item">
+            <a href="#0" class="list-slugs__link">Motokar Honda</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <ul class="list-slugs">
+          <li class="list-slugs__item">
+            <a href="#0" class="list-slugs__link">Encuentra tu repuesto</a>
+          </li>
+          <li class="list-slugs__item">
+            <a href="#0" class="list-slugs__link">Servicio técnico automotriz</a>
+          </li>
+          <li class="list-slugs__item">
+            <a href="#0" class="list-slugs__link">Comunidad Motera</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-md-3">
+        <ul class="list-slugs ps-md-0">
+          <li class="list-slugs__item">
+            <a href="#0" class="list-slugs__link">Términos y condiciones</a>
+          </li>
+        </ul>
+        <p class="text-white mb-1">Si deseas recibir más información dejanos tu email</p>
+        <?= do_shortcode('[contact-form-7 id="2500cac" title="Deseo más informacion"]'); ?>
+      </div>
+    </div>
+  </div>
+</footer>
+</body>
+
+</html>
